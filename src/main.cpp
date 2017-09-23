18c25cfc6b8a915798e90e3885d108e8754ff571
LibLib <liamdawson4751@gmail.com>
2017-09-23 16:50:40 +0100

    Added new drawing routine and textures

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "model.h"
 
 #include <iostream>
+#include <string>
 
 // Constants
 const TGAColour white = TGAColour(255, 255, 255, 255);
@@ -14,27 +15,21 @@ const int HEIGHT = 600;
 void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColour colour);
 void drawWireframe(Model *model, TGAImage &image);
 void drawModel(Model *model, TGAImage &image);
-void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour);
-void drawTriangleTop(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour);
-void drawTriangleBottom(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour);
+void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse);
+void drawTriangleTop(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse);
+void drawTriangleBottom(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse);
 void resetZBuf();
+bool loadTexture(std::string filename, TGAImage& texture);
 
+// Globals
 float zbuf[WIDTH * HEIGHT];
 bool isDebugging = false;
 
 int main() 
 {
-	TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
-
 	resetZBuf();
 
-	Vec3f A = Vec3f(110, 120, 0.512);
-	Vec3f B = Vec3f(120, 150, 0.581);
-	Vec3f C = Vec3f(130, 130, 0.612);
-	Vec3f D = Vec3f(150, 160, 0.654);
-
-	//drawTriangle(A, B, C, image, white);
-	//drawTriangle(B, C, D, image, red);
+	TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
 
 	Model model("obj/african_head.obj");
 	drawModel(&model, image);
@@ -50,35 +45,52 @@ void drawModel(Model *model, TGAImage &image)
 {
 	Vec3f lightDir(0, 0, -1);
 
+
+	TGAImage texture;
+	loadTexture("textures/head_texture.tga", texture);
+
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
 		Vec3f screen_points[3];
 		Vec3f world_points[3];
+		Vec2i texture_coords[3];
+
 		for (int j = 0; j < 3; j++)
 		{
 			Vec3f v = model->vert(face[j]);
 			screen_points[j] = Vec3f((v.x + 1.) * WIDTH / 2., (v.y + 1.) * HEIGHT / 2., v.z); // Convert to screen space
-			world_points[j] = v;
+			world_points[j] = v; // Get raw obj values (-1 to 1)
 		}
+
 		// Diffuse Lighting
 		Vec3f norm = (world_points[2] - world_points[0]) ^ (world_points[1] - world_points[0]); // Get orthagonal vector (normal)
 		norm.normalize();
+
 		float diffuse = norm * lightDir;
+
 		if (diffuse > 0)
-			drawTriangle(screen_points[0], screen_points[1], screen_points[2], image, TGAColour(255 * diffuse, 255 * diffuse, 255 * diffuse, 255));
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				texture_coords[j] = model->getTexCoord(i, j, texture);
+			}
+
+			drawTriangle(screen_points[0], screen_points[1], screen_points[2], texture_coords[0], texture_coords[1], texture_coords[2], image, texture, diffuse);
+		}
 	}
 }
 
-
-// Using "II. Standard Algorithm" found here: http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html
-// With some modifications
-void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour)
+void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse)
 {
-	Vec3f points[3] = { v0, v1, v2 };
-	Vec3f temp;
 
-	// Sort Verticies (top to bottom)
+
+	Vec3f points[3] = { v0, v1, v2 };
+	Vec2i texture_coords[3] = { vt0, vt1, vt2 };
+	Vec3f temp;
+	Vec2i tempt;
+
+	// Sort Verticies (top to bottom) via bubblesort
 	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 0; j < 2; j++)
@@ -88,6 +100,10 @@ void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colou
 				temp = points[j];
 				points[j] = points[j + 1];
 				points[j + 1] = temp;
+
+				tempt = texture_coords[j];
+				texture_coords[j] = texture_coords[j + 1];
+				texture_coords[j + 1] = tempt;
 			}
 		}
 	}
@@ -96,123 +112,153 @@ void drawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colou
 	v1 = points[1];
 	v2 = points[2];
 
-	// If flat on top
-	if (points[0].y == points[1].y)
+	if ((int)(v0.y + 0.5f) == (int)(v2.y + 0.5f))
+		return;
+
+	vt0 = texture_coords[0];
+	vt1 = texture_coords[1];
+	vt2 = texture_coords[2];
+
+	if (points[0].y == points[1].y) // If flat on top
 	{
-		drawTriangleBottom(points[0], points[1], points[2], image, colour);
+		drawTriangleBottom(points[0], points[1], points[2], texture_coords[0], texture_coords[1], texture_coords[2], image, texture, diffuse);
 	}
-	// If flat on bottom
-	else if(points[1].y == points[2].y)
+	else if(points[1].y == points[2].y) // If flat on bottom
 	{
-		drawTriangleTop(points[0], points[1], points[2], image, colour);
+		drawTriangleTop(points[0], points[1], points[2], texture_coords[0], texture_coords[1], texture_coords[2], image, texture, diffuse);
 	}
-	// Else triangle needs to be split
-	else
+	else 	// Else triangle needs to be split
 	{
 		// Find new vertex to split triangle
 		float x = v0.x + ((v1.y - v0.y) / (v2.y - v0.y)) * (v2.x - v0.x);
 		float z = v0.z + ((v1.y - v0.y) / (v2.y - v0.y)) * (v2.z - v0.z);
+		
+		int tx;
 
-		temp = { x, v1.y, z };
-		drawTriangleTop(v0, temp, v1, image, colour);
-		drawTriangleBottom(v1, temp, v2, image, colour);
+		if (vt2.y - vt0.y > 0)
+		{
+			tx = vt0.x + ((vt1.y - vt0.y) / (vt2.y - vt0.y)) * (vt2.x - vt0.x);
+		}
+		else
+		{
+			tx = vt0.x;
+		}
+
+		temp = { x, v1.y, z }; // New vertex
+		tempt = { tx, vt1.y }; // New texture coords
+
+		drawTriangleTop(v0, temp, v1, vt0, tempt, vt1, image, texture, diffuse);
+		drawTriangleBottom(v1, temp, v2, vt1, tempt, vt2, image, texture, diffuse);
 	}
 }
 
-void drawTriangleTop(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour)
+void drawTriangleTop(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse)
 {
 	// Make sure are left and right
 	if (v1.x > v2.x)
 		std::swap(v1, v2);
 
-	float left_slope;
-	float right_slope;
-	float x_start = v1.x;
-	float x_end = v2.x;
-	int y_top = v0.y;
-	int y_bottom = v2.y;
-	float z_left_slope;
-	float z_right_slope;
-	float z_end = v2.z;
-	float z_start = v1.z;
-	float current_z = z_start;
+	//     v0
+	//    |  |
+	//  |      |
+	// v1-------v2
 
-	// Find left and right slopes
-	left_slope = (v0.x - v1.x) / (y_top - y_bottom); 
-	right_slope = (v0.x - v2.x) / (y_top - y_bottom); 
-	z_left_slope = (v0.z - v1.z) / (y_top - y_bottom);
-	z_right_slope = (v0.z - v2.z) / (y_top - y_bottom);
+	int height = (int)v0.y - (int)v1.y; // Cast to int so they subtract as ints
 
-	// Draw scanlines across each y to fill triangle
-	for (int y = y_bottom; y <= y_top; y++)
+	if (height == 0)
+		return;
+
+	Vec3f start = v1;
+	Vec3f end = v2;
+
+	Vec2i tex_start = vt1;
+	Vec2i tex_end = vt2;
+	Vec2i tex_coord = vt1;
+
+	float z = start.z;
+	float y_factor; // How far into y of triangle (0 to 1)
+
+	for (int y = v1.y; y <= v0.y; y++)
 	{
-		for (int x = x_start; x <= x_end; x++)
+		y_factor = (y - (int)v1.y) / (float)height;
+
+		start = v1 + ((v0 - v1) * y_factor); // Cast to int or float because of how it loops through y
+		end = v2 + ((v0 - v2) * y_factor);
+
+		z = start.z;
+
+		tex_start = vt1 + ((vt0 - vt1) * y_factor);
+		tex_end = vt2 + ((vt0 - vt2) * y_factor);
+
+		tex_coord = tex_start;
+
+		for (int x = start.x; x <= end.x; x++)
 		{
-			if ((int)zbuf[(y * WIDTH) + x] * 100 < (int)current_z * 100) // Force 3 dp comparison
+			if ((int)zbuf[(y * WIDTH) + x] * 100 < (int)z * 100)
 			{
-				image.set(x, y, colour);
-				zbuf[(y * WIDTH) + x] = current_z;
-			};
-			current_z += (z_end - z_start) / ((int)x_end - (int)x_start);
+				TGAColour c = texture.get(tex_coord.x, tex_coord.y);
+				image.set(x, y, TGAColour(c.r * diffuse, c.g * diffuse, c.b * diffuse, c.a));
+				zbuf[(y * WIDTH) + x] = z;
+			}
+
+			z = start.z + ((end.z - start.z) * ((x - start.x) / (end.x - start.x))); // Find z along line
+			tex_coord = tex_start + ((tex_end - tex_start) * ((x - start.x) / (end.x - start.x)));
 		}
-
-		// Apply slopes
-		x_start += left_slope;
-		x_end += right_slope;
-
-		z_start += z_left_slope;
-		z_end += z_right_slope;
-
-		current_z = z_start;
 	}
 }
 
 
-void drawTriangleBottom(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage &image, TGAColour colour)
+void drawTriangleBottom(Vec3f v0, Vec3f v1, Vec3f v2, Vec2i vt0, Vec2i vt1, Vec2i vt2, TGAImage &image, TGAImage texture, float diffuse)
 {
 	// Make sure are left and right
 	if (v0.x > v1.x)
 		std::swap(v0, v1);
 
-	float left_slope;
-	float right_slope;
-	float x_start = v2.x;
-	float x_end = v2.x;
-	int y_top = v0.y;
-	int y_bottom = v2.y;
-	float z_left_slope;
-	float z_right_slope;
-	float z_start = v2.z;
-	float z_end = v2.z;
-	float current_z = z_start;
+	// V0 ----- v1
+	//   |     |
+	//     | |
+	//      v2
 
-	// Find left and right slopes
-	left_slope = (v0.x - v2.x) / (y_top - y_bottom); 
-	right_slope = (v1.x - v2.x) / (y_top - y_bottom);
-	z_left_slope = (v0.z - v2.z) / (y_top - y_bottom);
-	z_right_slope = (v1.z - v2.z) / (y_top - y_bottom);
+	int height = (int)v0.y - (int)v2.y;
 
-	// Draw scanlines across each y to fill triangle
-	for (int y = y_bottom; y <= y_top; y++)
+	if (height == 0)
+		return;
+
+	Vec3f start = v2;
+	Vec3f end = v2;
+
+	Vec2i tex_start = vt2;
+	Vec2i tex_end = vt2;
+	Vec2i tex_coord = vt2;
+
+	float z;
+	float y_factor;
+
+	for (int y = v2.y; y <= v0.y; y++)
 	{
-		for (int x = x_start; x <= x_end; x++)
+		y_factor = (y - (int)v2.y) / (float)height;
+
+		start = v2 + ((v0 - v2) * y_factor); // New start position
+		end = v2 + ((v1 - v2) * y_factor); // New end position
+		z = start.z;
+
+		tex_start = vt2 + ((vt0 - vt2) * y_factor); // New texture start position
+		tex_end = vt2 + ((vt1 - vt2) * y_factor); // New texture end position
+
+		tex_coord = tex_start;
+
+		for (int x = start.x; x <= end.x; x++)
 		{
-			if ((int)zbuf[(y * WIDTH) + x] * 100 < (int)current_z * 100)
+			if ((int)zbuf[(y * WIDTH) + x] * 100 < (int)z * 100)
 			{
-				image.set(x, y, colour);
-				zbuf[(y * WIDTH) + x] = current_z;
+				TGAColour c = texture.get(tex_coord.x, tex_coord.y);
+				image.set(x, y, TGAColour(c.r * diffuse, c.g * diffuse, c.b * diffuse, c.a));
+				zbuf[(y * WIDTH) + x] = z;
 			}
-			current_z += (z_end - z_start) / ((int)x_end - (int)x_start);
+
+			z = start.z + ((end.z - start.z) * ((x - start.x) / (end.x - start.x))); // Find z along line
+			tex_coord = tex_start + ((tex_end - tex_start) * ((x - start.x) / (end.x - start.x)));
 		}
-
-		// Apply slopes
-		x_start += left_slope;
-		x_end += right_slope;
-
-		z_start += z_left_slope;
-		z_end += z_right_slope;
-
-		current_z = z_start;
 	}
 }
 
@@ -288,5 +334,20 @@ void resetZBuf()
 		{
 			zbuf[(y * HEIGHT) + x] = -1.0f;
 		}
+	}
+}
+
+bool loadTexture(std::string filename, TGAImage& texture)
+{
+	if (texture.read_tga_file(filename.c_str()))
+	{
+		std::cout << filename << " loaded successfully" << std::endl;
+		texture.flip_vertically();
+		return true;
+	}
+	else
+	{
+		std::cout << filename << " unable to load" << std::endl;
+		return false;
 	}
 }
